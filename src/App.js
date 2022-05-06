f8658164cf2c873972af045b8aa19d5f71574daf
unknown <ra.nirmalkumar96@gmail.com>
2022-05-06 10:27:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,136 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './Navbar';
+import Header from './Header';
+import Footer from './Footer';
+import Card from './Card';
 
 function App() {
-  return (
+  const[Data,SetData] = useState([
+    {
+      id:1,
+      name: "Fancy product",
+      oldprice: 40.00,
+      newprice: 80.00,
+      isbadge:false,
+      israting:false,
+      isbtnname:"View options",
+      image:"https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+    },
+    {
+      id:2,
+      name: "Special Item",
+      oldprice: "$20.00",
+      newprice: 18.00,
+      isbadge:true,
+      israting: true,
+      isbtnname:"Add to cart",
+      image:"https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+    },
+    {
+      id:3,
+      name: "Sale Item",
+      oldprice: "$50.00",
+      newprice: 25.00,
+      isbadge:false,
+      israting: false,
+      isbtnname:"Add to cart",
+      image:"https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+
+    },
+    {
+      id:4,
+      name: "Popular Item",
+      oldprice: "",
+      newprice: 40.00,
+      isbadge: false,
+      israting: true,
+      isbtnname:"Add to cart",
+      image:"https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+    },
+    {
+      id:5,
+      name: "Sale Item",
+      oldprice: 50.00,
+      newprice: 25.00,
+      isbadge: false,
+      israting: false,
+      isbtnname:"Add to cart",
+      image:"https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+    },
+    {
+      id:6,
+      name: "Fancy product",
+      oldprice: 120.00,
+      newprice: 280.00,
+      isbadge: false,
+      israting: false,
+      isbtnname:"Add to cart",
+      image:"https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+    },
+    {
+      id:7,
+      name: "Special Item",
+      oldprice: "$20.00",
+      newprice: 18.00,
+      isbadge:true,
+      israting: true,
+      isbtnname:"Add to cart",
+      image:"https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+
+    },
+    {
+      id:8,
+      name: "Popular Item",
+      oldprice: "",
+      newprice: 40.00,
+      isbadge: false,
+      israting: true,
+      isbtnname:"Add to cart",
+      image:"https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+    }]);
+    const [Cart,setcart] = useState ([]);
+    const [total,settotal] = useState (0);
+
+    let Addtocart = (items) => {
+      setcart([...Cart,items])
+      settotal (total+items.newprice)
+    }
+    console.log (Addtocart);
+
+    let Removecart = (items) => {
+      let itemindex = Cart.findIndex(obj => items.id === obj.id);
+      Cart.splice(itemindex,1);
+      setcart([...Cart])
+      settotal (total- items.newprice);
+    }
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light"> 
+      <Navbar Cart = {Cart} Removecart ={Removecart} total = {total}></Navbar>
+      </nav>
+      <header class="bg-dark py-5">
+        <Header></Header>
       </header>
-    </div>
+      <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                  {
+                    Data.map((Product)=>{
+                      return <Card Product = {Product} Addcart= {Addtocart} Cart={Cart}></Card>
+                    })
+                  }
+                  </div>
+                  </div>
+                  </section>
+
+  
+        <Footer></Footer>
+    
+
+</div>
   );
 }
 
---
+++ src/Cart.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+function  Cartcard(props){
+    return (
+        <div>
+        <ol class="list-group">
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">{props.items.name} - ${props.items.newprice}</div>
+      
+    </div>
+    <button onClick={() => {
+        props.Removecart(props.items)
+    }} class="badge bg-primary rounded-pill" type="button">Remove</button>
+  </li>
+</ol>
+    </div>
+  )
+}
+
+export default Cartcard;
