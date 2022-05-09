e517f6a65c170aaaf9af625580d97d1300e4ef12
unknown <ra.nirmalkumar96@gmail.com>
2022-05-09 14:35:56 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import './App.css';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+//import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './Navbar';
 import Header from './Header';
 import Footer from './Footer';
--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+//import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 function Footer (){
     return(
         <footer class="py-5 bg-dark">
